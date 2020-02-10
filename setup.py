d38f8d480f6500d51aa8e8e7b23c682341a627c2
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-02-10 16:20:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'security-python',         # How you named your package folder (MyLib)
-  packages = ['security-python'],   # Chose the same as "name"
+  name = 'security_python',         # How you named your package folder (MyLib)
+  packages = ['security_python'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Security Module for python',   # Give a short description about your library
   author = 'JonathanG',                   # Type in your name
   author_email = 'ceo@graffbt.com',      # Type in your E-Mail
   url = 'https://github.com/zurgeg/security-python',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zurgeg/security-python/master/v1_1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/zurgeg/security-python/master/v1_2.tar.gz',    # I explain this later on
   keywords = ['Security', 'Passwords', 'Hashing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'werkzeug'
       ],
   classifiers=[
-    'Development Status :: 4 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.8',
